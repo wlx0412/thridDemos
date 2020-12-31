5d7207dcfaafba4dd9dcfb6aec2f5dec38998b85
wlx <164305171@qq.com>
2020-12-31 09:55:51 +0800

    create

---
+++ thridDemos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  thridDemos
+//
+//  Created by wlx on 2020/12/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ thridDemosTests/thridDemosTests.swift
@@ -0,0 +1,33 @@
+//
+//  thridDemosTests.swift
+//  thridDemosTests
+//
+//  Created by wlx on 2020/12/31.
+//
+
+import XCTest
+@testable import thridDemos
+
+class thridDemosTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
